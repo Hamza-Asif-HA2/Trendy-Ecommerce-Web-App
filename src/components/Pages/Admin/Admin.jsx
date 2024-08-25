d007605b8e39666d79981e2c6e2a223577be6a83
Hamza-Asif-HA2 <hamzaasif0726@gmail.com>
2024-08-26 03:32:01 +0500

    minor change

--- src/components/Pages/Admin/Admin.jsx
+++ src/components/Pages/Admin/Admin.jsx
@@ -4,7 +4,9 @@ const Admin = () => {
  
 
   return (
-   <div className="max-w-7xl mx-auto w-full">
+   <div className="max-w-7xl mx-auto w-full m-2">
+    <div className='bg-gray-300 p-4 rounded-2xl'>
+
     <h1 className="text-center m-2 text-4xl font-bold uppercase">Admin Dashboard</h1>
     <h6 className="text-center text-gray-600 font-bold">Manage Products here</h6>
     <div className="m-8 row flex space-x-4 justify-center">
@@ -14,6 +16,7 @@ const Admin = () => {
       <div className="p-4 border border-solid rounded-lg text-2xl m-2 font-bold bg-orange-600 text-white">Orders</div>
           
     </div>
+     </div>
     <Outlet />
    </div>
 
